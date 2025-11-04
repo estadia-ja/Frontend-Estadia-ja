a05829a2887379c3c6493ea4ee8f9eaba098f9cd
pkbceira03 <cabeceira2003@gmail.com>
2025-11-04 11:16:34 -0300

    feat: adiciona Home e esquena inicial das routes.

--- estadia-ja-frontend/src/App.tsx
+++ estadia-ja-frontend/src/App.tsx
@@ -1,9 +1,19 @@
-import { SignUp } from "./pages/SignUp"
+// src/App.jsx
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import { Home } from "./pages/Home/Home.tsx"
+import { SignUp } from "./pages/SignUp/index.tsx"
+import { Login } from "./pages/Login/index.tsx"
 
-export function App(){
+function App() {
   return (
-    <>
-      <SignUp/>
-    </>
+    
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<SignUp />} />
+    </Routes>
   )
 }
+
+export default App
