668112faf97241454757f9ef97d8bdeafe3d416d
pkbceira03 <cabeceira2003@gmail.com>
2025-11-06 09:42:34 -0300

    Fix: corrige erro na configuração do lint.

---
+++ estadia-ja-frontend/eslint.config.js
@@ -0,0 +1,47 @@
+import globals from "globals";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginReactRefresh from "eslint-plugin-react-refresh";
+import prettierConfig from "eslint-config-prettier";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "dist/**",
+      "node_modules/**",
+      "eslint.config.js",
+      "vite.config.ts"
+    ],
+  },
+
+  eslint.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["src/**/*.{ts,tsx}"], 
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true }, 
+      },
+      globals: {
+        ...globals.browser, 
+        ...globals.es2020,  
+      },
+    },
+    plugins: {
+      "react-hooks": pluginReactHooks,
+      "react-refresh": pluginReactRefresh,
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules, 
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+
+  prettierConfig
+);
