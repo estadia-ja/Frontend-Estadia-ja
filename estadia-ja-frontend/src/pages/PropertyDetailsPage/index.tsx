17d3af91b5fee316b5235cc895d5cbe664d84f9b
pkbceira03 <cabeceira2003@gmail.com>
2025-11-13 16:13:19 -0300

    fix: coloca o import react onde precisa.

--- estadia-ja-frontend/src/pages/PropertyDetailsPage/index.tsx
+++ estadia-ja-frontend/src/pages/PropertyDetailsPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
 import axios from 'axios';
 import { Star, Heart } from 'lucide-react';
